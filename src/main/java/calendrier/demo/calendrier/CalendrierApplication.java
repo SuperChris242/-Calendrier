76d0f2ac1f23a772b2050fb182ec8ceea3504873
SuperChris242 <okoueretchristian@gmail.com>
2023-12-04 23:31:51 +0100

    Code source initial

---
+++ src/main/java/calendrier/demo/calendrier/CalendrierApplication.java
@@ -0,0 +1,13 @@
+package calendrier.demo.calendrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/calendrier/demo/calendrier/CalendrierApplicationTests.java
@@ -0,0 +1,13 @@
+package calendrier.demo.calendrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
